ac0c58cabe34a87ef43b4cbcbc4e2ae829bd7f60
sameerkarim <sameerkarim1@gmail.com>
2012-04-29 23:13:15 +0800

    Added a title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,15 +4,21 @@ require 'spec_helper'
     
     describe "Home page" do
       
-      it "should have the content 'Originate China HR Tracker'" do
+      it "should have the h1 'Originate China HR Tracker'" do
         visit '/static_pages/home'
         page.should have_selector('h1', :text => 'Originate China HR Tracker')
       end
     
-      it "should have the title 'Home'" do
+      it "should have the base title" do
         visit '/static_pages/home'
         page.should have_selector('title', 
-                                  :text => "Originate China HR Tracker | Home")
+                                  :text => "Originate China HR Tracker")
+      end
+      
+      it "should not have a custom page title" do
+        visit '/static_pages/home'
+        page.should_not have_selector('title', 
+                                      :text => "| Home")
       end
     end
   
