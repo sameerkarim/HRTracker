2a92ba4851fd1c30c76b0c1733934a30c9aa1981
sameerkarim <sameerkarim1@gmail.com>
2012-04-30 01:41:54 +0800

    Finished layouts and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,39 +2,24 @@ require 'spec_helper'
 
   describe "StaticPages" do
     
-    describe "Home page" do
-      
-      it "should have the h1 'Originate China HR Tracker'" do
-        visit '/static_pages/home'
-        page.should have_selector('h1', :text => 'Originate China HR Tracker')
-      end
+    subject { page }
     
-      it "should have the base title" do
-        visit '/static_pages/home'
-        page.should have_selector('title', 
-                                  :text => "Originate China HR Tracker")
-      end
+    describe "Home page" do
+      before { visit root_path }
       
-      it "should not have a custom page title" do
-        visit '/static_pages/home'
-        page.should_not have_selector('title', 
-                                      :text => "| Home")
-      end
+      it { should have_selector('h1', :text => 'Originate China HR Tracker') }
+      it { should have_selector('title', 
+                                  :text => "Originate China HR Tracker") }
+      it { should_not have_selector('title', 
+                                      :text => "| Home") }
     end
   
     describe "About page" do
+      before {visit about_path}
       
-      it "should have the content 'About Originate China\'s HR Tracker'" do
-        visit '/static_pages/about'
-        page.should have_selector('h1', :text => "About Originate China's HR Tracker")        
-      end
-      
-      it "should have the title 'About'" do
-         visit '/static_pages/about'
-         page.should have_selector('title', 
-                                   :text => "Originate China HR Tracker | About")
-      end
+      it{ should have_selector('h2', :text => "About Originate China's HR Tracker") }      
+      it { should have_selector('title', 
+                                   :text => "Originate China HR Tracker | About") }
     end
-  
   
 end
