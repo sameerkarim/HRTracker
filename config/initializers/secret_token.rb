7be879a96f6e5dc9a1c4c132fd61fd23be257af6
sameerkarim <sameerkarim1@gmail.com>
2012-04-29 14:57:30 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OCHRTracker::Application.config.secret_token = '2efc7c913a0744434a77461737eb5865b2117d3fcce708456e24ee5923d2a428ee256f579038095524e02635f8888ae11af0fa11d77d0764d83529cfad02ea6d'
